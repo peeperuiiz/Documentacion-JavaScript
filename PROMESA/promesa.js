0d7106dff66e637f2e8320bc3cba21ac3f8a687a
Pepe <pruiz2763@gmail.com>
2024-11-19 10:34:47 +0100

    Explicación promesa.js

--- PROMESA/promesa.js
+++ PROMESA/promesa.js
@@ -1,1 +1,18 @@
 
+let promesa = new Promise((resolve, reject) => {                                        //SIEMPRE DEBE TENER AL MENOS UNO DE LOS PARÁMETROS
+
+    let resuelto = false;
+
+    if(resuelto){
+        resolve([1,2,3,4,5]);
+    }else{
+        reject("Error");
+    }
+});
+
+promesa
+.then((datos) => {                                                                      //SIEMPRE SE ESCRIBE CON .then YA QUE TENEMOS QUE EJECUTAR ESTO TRAS LA PROMESA Y HACEMOS UN CALLBACK DE DATOS
+    console.log(datos);
+}).catch(error => {                                                                     //CONTROLAMOS LA EXCEPCIÓN O ERROR
+    console.log(error)
+});
