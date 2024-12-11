662f5b80eea53a8c7fe8b603d8bb317b9cdd84c6
Pepe <pruiz2763@gmail.com>
2024-12-11 10:27:42 +0100

    Añadida capa de seguridad

--- CONEXIÓN A BASES DE DATOS/APUNTES INICIALES/insertar.php
+++ CONEXIÓN A BASES DE DATOS/APUNTES INICIALES/insertar.php
@@ -1,17 +1,28 @@
 <?php
 
     require_once("includes/conexion.php");
+    $header = getallheaders();
 
-    if (isset($_POST["titulo"], $_POST["texto"], $_POST["img"])) {
-        $tituloCurso = $_POST["titulo"];
-        $textoCurso = $_POST["texto"];
-        $imgCurso = $_POST["img"];
+    if(substr($header['Authorization'], 7) === "0123456789"){
 
-        $consulta = "insert into cursos (titulo, texto, img) values (?,?,?)";
-        $sentencia = $conexion->prepare($consulta);
-        $sentencia->execute([$tituloCurso, $textoCurso, $imgCurso]);
+        if (isset($_POST["titulo"], $_POST["texto"], $_POST["img"])) {
+            $tituloCurso = $_POST["titulo"];
+            $textoCurso = $_POST["texto"];
+            $imgCurso = $_POST["img"];
+    
+            $consulta = "insert into cursos (titulo, texto, img) values (?,?,?)";
+            $sentencia = $conexion->prepare($consulta);
+            $sentencia->execute([$tituloCurso, $textoCurso, $imgCurso]);
+    
+            if($sentencia){
+                echo json_encode("true");
+            }
+        }else{
+            header("Location:index.html");
+        }
+
     }else{
-        header("Location:index.html");
+
     }
 
 ?>
